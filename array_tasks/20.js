eb939119f121fa8045c3551d1f5816a6b78df126
milanalakatosh <milanammk@gmail.com>
2021-05-25 11:11:59 +0300

    Update 20.js

--- array_tasks/20.js
+++ array_tasks/20.js
@@ -2,20 +2,26 @@
 const readlineSync = require("readline-sync");
 
 let array = JSON.parse(readlineSync.question("Введите массив:\n> "));
-let replacement = true;
-let moreThenOneOfreplacement = false;
 
-for (let i = 0; i < array.length - 1; ++i) {
-    if (array[i] > array[i+1]) {
-        if (!moreThenOneOfreplacement) { //|| array[i] > array[i+2]) {
-            replacement = true;
-            moreThenOneOfreplacement = true;
-            //break;
-        } else //moreThenOneOfreplacement = true; {
-            replacement = false;
+let removed = false;
+let mayRemove = true;
+let increasing = true;
+
+for (let i = 0; i < array.length - 2; ++i) {
+    if (array[i] >= array [i+1]) {
+        increasing = false;
+        if (array[i+2] > array[i] || i === 0) {
+            if (!removed) removed = true;
+            else {
+                mayRemove = false;
+                break;
+            }
+        } else if (array[i+2] <= array[i]) {
+            mayRemove = false;
+            break;
         }
     }
-
-
-console.log(replacement ? "Из массива можно вычеркнуть 1 число так, чтобы оставшиеся числа оказались упорядоченными по возрастанию."
+}
+console.log(increasing ? "Массив и так возрастающий"
+    : mayRemove ? "Из массива можно вычеркнуть 1 число так, чтобы оставшиеся числа оказались упорядоченными по возрастанию."
     : "Из массива нельзя вычеркнуть 1 число так, чтобы оставшиеся числа оказались упорядоченными по возрастанию.");
