bcc899b92bc13a22f70b403f0f0d30fed7262b32
milanalakatosh <milanammk@gmail.com>
2021-07-24 11:40:21 +0700

    27.07.21

--- libraries_regex/8.3.js
+++ libraries_regex/8.3.js
@@ -3,9 +3,9 @@
 import * as readlineSync from "readline-sync";
 
 const str = readlineSync.question("String?\n> ");
-//one Two Three
+
 let re = /a-z, [^/s]/;//или х?
 let re2 = /X/, [^/s]/;
 let a1 = re.exec(str);
-//let small = 0;
+
 console.log(a1.length);
