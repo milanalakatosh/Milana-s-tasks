a561cb19076172318a1e09076ec5356c2b902d9e
milanalakatosh <milanammk@gmail.com>
2021-10-09 05:30:32 +0300

    09.10

--- dom/examples/note/1.js
+++ dom/examples/note/1.js
@@ -6,7 +6,9 @@ const listNotes = [
     {head: "Text3", text: "Lorem Loremcdmfnmd"}
 ];
 const notes = document.querySelector(".notes");
+
 for (const note of listNotes) {
+    
     const div = document.createElement("div");
     div.classList.add("note");
     div.textContent=note.head;
@@ -16,5 +18,16 @@ for (const note of listNotes) {
         head.value = note.head;
         const innerText = document.querySelector(".inner-text");
         innerText.textContent=note.text;
+        const noteActive = document.querySelector(".note.active");
+        if (noteActive !== null) noteActive.classList.remove("active");
+        div.classList.add("active");
     });
 }
+
+const buttonAdd = document.querySelector(".add");
+buttonAdd.addEventListener("click", () => listNotes.push({head: "New Note", text: ""}));
+
+const buttonDelete = document.querySelector(".delete");
+buttonDelete.addEventListener("click", () => {
+    notes.removeChild()
+})
