38ca7999c7cf564ad227ccf48358f6603027de07
abakharev <abakharev@sharevault.com>
2021-11-21 14:25:29 +0700

    regexp

--- regex/samples.js
+++ regex/samples.js
@@ -1,16 +1,16 @@
-// // проверить соответствует ли строка регэкспу
-// // вся строка
+// проверить соответствует ли строка регэкспу
+// вся строка
 const s1 = "abc";
 console.log(/^abc$/.test(s1));
 console.log(/^a.c$/.test(s1));
 console.log(/^a\dc$/.test(s1));
 
-// // одно вхождение в строку
+// одно вхождение в строку
 const s2 = "abc def ghi";
 console.log(/e/.test(s2));
 console.log(/d.f/.test(s2));
 
-// // найти вхождение в строку
+// найти вхождение в строку
 const s3 = "abc def ghi";
 const result3 = /d.f/.exec(s3);
 console.log(result3);
@@ -18,7 +18,7 @@ console.log(result3[0]);
 console.log(result3.index);
 console.log(result3.input);
 
-// // найти несколько вхождений (флаг g)
+// найти несколько вхождений (флаг g)
 const s4 = "abc def dbf";
 console.log(s4.match(/d.f/g));
 
