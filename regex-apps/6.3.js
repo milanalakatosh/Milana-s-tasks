2da455868b90ead46bd1faaef906f2c45ea9b5a8
milanalakatosh <milanammk@gmail.com>
2022-01-26 14:45:18 +0200

    1

--- regex-apps/6.3.js
+++ regex-apps/6.3.js
@@ -2,29 +2,26 @@
 const text = document.querySelector(".text");
 const textArea = document.querySelector(".text-area");
 const rightSide = document.querySelector(".right.side");
-let buttonDelete;//норм так через let??
 let labelRadioMails = [];
 let domElMails;
 const okButton = document.querySelector("#ok");
 const editButton = document.querySelector("#edit");
 
-let str = "  Смысл вашего mail3@gmail.com сообщения mail@mail.ru должен легко считываться и находить отклик у читающего. Ведь качественный текст и хорошее оформление mail2@yandex.ru не сильно помогут, если вы пишите о химическом составе лака для ногтей человеку, которому гораздо mail3@gmail.com интереснее узнать о палитре модных оттенков на лето 2021";
-
 const regex = /\w+([-.]\w+)*@\w+([-.]\w+)*\.\w{2,}/g;
 
-runProgram(text, regex);
+runProgram("  Смысл вашего mail3@gmail.com сообщения mail@mail.ru должен легко считываться и находить отклик у читающего. Ведь качественный текст и хорошее оформление mail2@yandex.ru не сильно помогут, если вы пишите о химическом составе лака для ногтей человеку, которому гораздо mail3@gmail.com интереснее узнать о палитре модных оттенков на лето 2021");
 
-
-function runProgram(text, regex) {
-    text.innerHTML = str.replace(regex, '<span class="mail">$&</span><span class="delete-icon"> &#10060;</span>');
-    exportMailsFromTextAndInstallDeleteIcon(text, regex);
-    switchButtons();
-    deleteItems();
-    editText(editButton, okButton);
+function runProgram(str) {
+    text.innerHTML = createHtmlForMails(str);
+    createMailsRadioGroups();
+    handleMailActivation();
+    handleMailRemoving();
+    handleTextEditing(str);
 }
-function exportMailsFromTextAndInstallDeleteIcon(text, regex) {
-    if (labelRadioMails.length > 0) rightSide.innerHTML = "";
-
+function createHtmlForMails(str) {
+    return str.replace(regex, '<span class="mail">$&</span><span class="delete-icon"> &#10060;</span>');
+}
+function createMailsRadioGroups() {
     const mails = text.textContent.match(regex);
     for (const mail of mails) {
         const label = document.createElement("label");
@@ -37,7 +34,7 @@ function exportMailsFromTextAndInstallDeleteIcon(text, regex) {
         rightSide.append(label);
         label.prepend(radioInput);
 
-        buttonDelete = document.createElement("input");
+        const buttonDelete = document.createElement("input");
         buttonDelete.type = 'button';
         buttonDelete.classList.add("delete");
         label.append(buttonDelete);
@@ -48,34 +45,33 @@ function exportMailsFromTextAndInstallDeleteIcon(text, regex) {
     }
     labelRadioMails = rightSide.querySelectorAll(".mailInColumn");
 }
-function setMailActive(clickedMail) {
+function setMailActiveInText(mail) {
     const mailActive = text.querySelector(".mail.active");
     if (mailActive) mailActive.classList.remove("active");
-    clickedMail.classList.add("active");
+    mail.classList.add("active");
 }
-function switchButtons() {
+function handleMailActivation() {
     domElMails = text.querySelectorAll(".mail");
     for (let j = 0; j < domElMails.length; ++j) {
         domElMails[j].addEventListener("click", () => {
-            setMailActive(domElMails[j]);
+            setMailActiveInText(domElMails[j]);
             const radioButton = labelRadioMails[j].querySelector("input");
             radioButton.checked = "true";
         });
         for (let i = 0; i < labelRadioMails.length; ++i) {
             const radioButton = labelRadioMails[i].querySelector("input");
             radioButton.addEventListener("change", () => {
-                setMailActive(domElMails[i]);
+                setMailActiveInText(domElMails[i]);
                 domElMails[i].classList.add("active");
             });
         }
     }
 }
-function deleteItems() {
+function handleMailRemoving() {
     const emojisInText = text.querySelectorAll(".delete-icon");
     const deleteIconOnLabelRadio = rightSide.querySelectorAll(".delete");
     for (let j = 0; j < deleteIconOnLabelRadio.length; ++j) {
         deleteIconOnLabelRadio[j].addEventListener("click", () => {
-            console.log(labelRadioMails);
             labelRadioMails[j].remove();
             domElMails[j].classList.add("unmarked");
             emojisInText[j].remove();
@@ -89,22 +85,22 @@ function deleteItems() {
         });
     }
 }
-function editText(editBtn, okBtn) {
-    editBtn.addEventListener("click", () => {
-        editBtn.style.display = "none";
-        okBtn.style.display = "inline-block";
+function handleTextEditing(str) {
+    editButton.addEventListener("click", () => {
+        rightSide.innerHTML = "";
+        editButton.style.display = "none";
+        okButton.style.display = "inline-block";
         text.style.display = "none";
         textArea.style.display = "inline-block";
         textArea.value = str;
     });
     
-    okBtn.addEventListener("click", () => {
-        okBtn.style.display = "none";
-        editBtn.style.display = "inline-block";
+    okButton.addEventListener("click", () => {
+        okButton.style.display = "none";
+        editButton.style.display = "inline-block";
         text.style.display = "inline-block";
         textArea.style.display = "none";
-        str = textArea.value;
-        runProgram(text, regex);
+        runProgram(textArea.value);
     });
 }
 
